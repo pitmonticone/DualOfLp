0603be5da15046fe34d52c5e3adb5fc918108178
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2025-12-06 10:21:22 +0100

    out of this world (with non compiling phi)

--- DualOfLp/Talk/filter_upwards.lean
+++ DualOfLp/Talk/filter_upwards.lean
@@ -2,9 +2,9 @@
 
 import Mathlib
 
-example : 2 = 2 := rfl
+section Filter
 
-open /- Filter -/ Topology
+open Filter Topology
 variable {α β γ : Type*}
 
 lemma mono' {f : α → β} {F G : Filter α} (h : F ≤ G) : F.map f ≤ G.map f := by
@@ -18,7 +18,7 @@ lemma mono' {f : α → β} {F G : Filter α} (h : F ≤ G) : F.map f ≤ G.map 
 def MyTendsto (f : α → β) (F : Filter α) (G : Filter β) := F.map f ≤ G
 
 /- Scrivendo fare l' errore `MyTendsto (g ∘ f) G H` per mostrare che lean e' sveglio-/
-lemma myTendsto_comp {F : Filter α} {G : Filter β} {H : Filter γ}
+lemma myTendsto_comp {f : α → β} {g : β → γ} {F : Filter α} {G : Filter β} {H : Filter γ}
     (hf : MyTendsto f F G) (hg : MyTendsto g G H) : MyTendsto (g ∘ f) F H :=
   (map_mono hf).trans hg
 
@@ -30,7 +30,7 @@ example (a : ℕ → ℝ) (φ : ℝ → ℂ) (ha : MyTendsto a atTop (𝓝 (-1))
     (hφ : MyTendsto φ (𝓝 (-1)) (𝓝 (Complex.I))) : MyTendsto (φ ∘ a) atTop (𝓝 (Complex.I)) := by
   apply myTendsto_comp ha hφ
 
-/--
+/-
 `filter_upwards [h₁, ⋯, hₙ]` replaces a goal of the form `s ∈ f` and terms
 `h₁ : t₁ ∈ f, ⋯, hₙ : tₙ ∈ f` with `∀ x, x ∈ t₁ → ⋯ → x ∈ tₙ → x ∈ s`.
 The list is an optional parameter, `[]` being its default value.
@@ -45,16 +45,16 @@ Combining both shortcuts is done by writing `filter_upwards [h₁, ⋯, hₙ] wi
 Note that in this case, the `aᵢ` terms can be used in `e`.
 -/
 
-example (ε : Fin 4 → {x : ℝ | 0 < x}) :
-    ⋂ (i : Fin 4), (Set.Ioo (- (ε i).1) (ε (i+1))) ∈ 𝓝 (0) := by
-  have : Set.Ioo (- (⨅ (i : Fin 4), (ε i).1)) (⨆ (i : Fin 4), (ε i)) ∈ 𝓝 (0) := sorry
-  filter_upwards [this] with x hx
-  simp only [Set.mem_Ioo, Fin.isValue, Set.mem_iInter] at ⊢ hx
-  intro i
-  constructor
-  · replace hx := hx.1
-    apply lt_trans _ hx
-    rw [neg_lt, neg_neg]
+-- example (ε : Fin 4 → {x : ℝ | 0 < x}) :
+--     ⋂ (i : Fin 4), (Set.Ioo (- (ε i).1) (ε (i+1))) ∈ 𝓝 (0) := by
+--   have : Set.Ioo (- (⨅ (i : Fin 4), (ε i).1)) (⨆ (i : Fin 4), (ε i)) ∈ 𝓝 (0) := sorry
+--   filter_upwards [this] with x hx
+--   simp only [Set.mem_Ioo, Fin.isValue, Set.mem_iInter] at ⊢ hx
+--   intro i
+--   constructor
+--   · replace hx := hx.1
+--     apply lt_trans _ hx
+--     rw [neg_lt, neg_neg]
 
 
 
@@ -67,3 +67,25 @@ example (ε : Fin 4 → {x : ℝ | 0 < x}) :
 --   filter_upwards-- [this]
   -- intro S
   -- filter_upwards [Filter.map_mem, eventually_gt_atTop 100]
+
+end Filter
+
+section Lp
+
+open ENNReal MeasureTheory
+
+variable (ι V : Type*) (E : ι → Type*) (p : ℝ≥0∞)
+variable [(i : ι) → NormedAddCommGroup (E i)] [MeasurableSpace V] (μ : Measure V)
+
+#check PreLp (α := ι) E
+#check PiLp p E
+
+#check @Lp V ℝ _ _ p (μ)
+#check Lp ℝ p μ
+
+example : PiLp p E = ((i : ι) → E i) := rfl
+example : PreLp (α := ι) E = ((i : ι) → E i) := rfl
+
+#check ae μ
+
+end Lp
